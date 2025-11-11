0bd11a02aee89400984c2afcded3f0e386ba32e2
Khong Jia Ren <jiaren.kjr@gmail.com>
2025-11-11 08:50:52 +0800

    eval_params: fixed initial set of hyper param ranges

--- R/utils_eval.R
+++ R/utils_eval.R
@@ -23,3 +23,44 @@ XGBOOST_PARAMS_REGRESSION <- list(
     gamma = c(0, 10),
     lambda = c(1e-3, 10)
 )
+
+XGBOOST_PARAMS_BINARY_CLASSIFICATION <- list(
+    eta = c(0.01, 0.3),
+    max_split_levels = c(3L, 10L),
+    min_node_size = c(2L, 20L),
+    bag_fraction = c(0.5, 1.0),
+    gamma = c(0, 10),
+    lambda = c(1e-3, 10)
+)
+
+GBM_PARAMS_REGRESSION <- list(
+    eta = c(0.01, 0.3),
+    max_split_levels = c(3L, 10L),
+    min_node_size = c(2L, 20L),
+    bag_fraction = c(0.5, 1.0)
+)
+
+GBM_PARAMS_BINARY_CLASSIFICATION <- list(
+    eta = c(0.01, 0.3),
+    max_split_levels = c(3L, 10L),
+    min_node_size = c(2L, 20L),
+    bag_fraction = c(0.5, 1.0)
+)
+
+CATBOOST_PARAMS_REGRESSION <- list(
+    eta = c(0.01, 0.3),
+    max_split_levels = c(3L, 10L),
+    min_node_size = c(2L, 20L),
+    bagging_temperature = c(0.5, 1.0),
+    rsm = c(0.5, 1.0),
+    l2_leaf_reg = c(0.5, 1.0)
+)
+
+CATBOOST_PARAMS_BINARY_CLASSIFICATION <- list(
+    eta = c(0.01, 0.3),
+    max_split_levels = c(3L, 10L),
+    min_node_size = c(2L, 20L),
+    bagging_temperature = c(0.5, 1.0),
+    rsm = c(0.5, 1.0),
+    l2_leaf_reg = c(0.5, 1.0)
+)
