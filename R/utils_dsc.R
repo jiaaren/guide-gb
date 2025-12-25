3318db7b4bb8b3cc657100d9c46d73782e51fc85
Khong Jia Ren <jiaren.kjr@gmail.com>
2025-12-25 10:20:04 +0800

    utils: updated DSC utils to consider factors

--- R/utils_dsc.R
+++ R/utils_dsc.R
@@ -5,14 +5,14 @@
 #   missing values = NA
 #   data would start at row 2
 #   begin constructing DSC file by guessing data types
-create_dsc <- function(df, output="", input_file='data.csv',
-                        missing_values='NA', data_start=2) {
+create_dsc <- function(df, output="", input_file="data.csv",
+                        missing_values="NA", data_start=2) {
     dsc_string <- paste(input_file, missing_values, data_start, sep="\n")
     cols <- c()
     for (i in 1:ncol(df)) {
         class_ <- class(df[,i])
-        if (class_ == 'numeric') id = 'n'
-        if (class_ == 'factor') id = 'c'
+        if (class_ == "numeric" || class_ == "integer") id = "n"
+        if (class_ == "factor" || class_ == "character") id = "c"
         cols[i] <- paste(i, colnames(df)[i], id)
     }
     dsc_string <- paste(dsc_string, "\n", paste(cols, collapse="\n"), "\n", sep="")
